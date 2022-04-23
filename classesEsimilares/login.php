994e0e1cc1d1b919fb8ef08e2ebb24e12da82176
crismgsp <crismgsp@gmail.com>
2022-04-23 12:54:24 -0300

    enviando controle do login um pouco mais arrumado

--- classesEsimilares/login.php
+++ classesEsimilares/login.php
@@ -1,14 +1,29 @@
 <?php
 
-require '../config.php'
+require '../config.php';
 
-if(empty($_POST['Nome'])) || empty($_POST['Senha'])) {
-    header('Location: paginalogin.php');
+if(empty($_POST['Nome']) || empty($_POST['Senha'])) {
+    header('Location: ../paginasvisualizacao/paginalogin.php');
     exit();
 }
 
 $Nome = mysqli_real_escape_string($mysql, $_POST['Nome']);
 $Senha = mysqli_real_escape_string($mysql, $_POST['Senha']);
 
+$query = "select * from usuarios where Nome ={$Nome} and Senha = {$Senha}';
 
-?>
+$result = mysqli_query($mysql, $query);
+
+$row = mysqli_num_rows($result);
+
+echo $row;exit;
+
+if($row ==1) {
+    $_SESSION[Nome] = $Nome;
+    header('Location: ../paginasadmin/importacoes.php')
+    exit();
+}else { 
+    header:('Location: ../paginasvisualizacao/paginalogin.php');
+    exit();   
+}
+
--- paginasvisualizacao/paginalogin.php
+++ paginasvisualizacao/paginalogin.php
@@ -13,7 +13,7 @@
         <title>Login</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="../assets/css/login.css">
 
 
     </head>    
