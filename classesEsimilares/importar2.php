3a41dcb14aa8d0ebbe3f117127ac6af25a2eff0c
crismgsp <crismgsp@gmail.com>
2022-05-04 05:52:21 -0300

    arrumei arquivo de importacao ..so insere com data diferente do banco

--- classesEsimilares/importar2.php
+++ classesEsimilares/importar2.php
@@ -97,45 +97,48 @@
     if (in_array($datap, $datasbanco)) {
         echo("Já tem esta data no banco");
     }else{
-        while(($dados = fgetcsv($objeto, 1000, ",")) !== FALSE)
-        {
-                   
-                    
-            $BancoOrigem = utf8_encode($dados[0]);
-            $AgenciaOrigem = utf8_encode($dados[1]);
-            $ContaOrigem = utf8_encode($dados[2]);
-            $BancoDestino = utf8_encode($dados[3]);
-            $AgenciaDestino = utf8_encode($dados[4]);
-            $ContaDestino = utf8_encode($dados[5]);
-            $Valor = utf8_encode($dados[6]);
-            $DataeHora = utf8_encode($dados[7]);
+
+        $objeto = fopen($arquivo, 'r');   
+
+            while(($dados = fgetcsv($objeto, 1000, ",")) !== FALSE)
+            {
+                
                         
-            $url = str_replace("Novo/", "", $_SERVER["REQUEST_URI"]);
+                $BancoOrigem = utf8_encode($dados[0]);
+                $AgenciaOrigem = utf8_encode($dados[1]);
+                $ContaOrigem = utf8_encode($dados[2]);
+                $BancoDestino = utf8_encode($dados[3]);
+                $AgenciaDestino = utf8_encode($dados[4]);
+                $ContaDestino = utf8_encode($dados[5]);
+                $Valor = utf8_encode($dados[6]);
+                $DataeHora = utf8_encode($dados[7]);
+                            
+                $url = str_replace("Novo/", "", $_SERVER["REQUEST_URI"]);
 
 
-            $explodeurl = explode("=", $url);
+                $explodeurl = explode("=", $url);
+
+                            
+                $usuariomodificado = $explodeurl[1];
+                                        
+                $usuariomodificado2 = str_replace("%27", " ", $usuariomodificado);
+
+                $usuario = str_replace("%20", " ", $usuariomodificado2);
 
                         
-            $usuariomodificado = $explodeurl[1];
-                                    
-            $usuariomodificado2 = str_replace("%27", " ", $usuariomodificado);
-
-            $usuario = str_replace("%20", " ", $usuariomodificado2);
-
-                    
-            $result = $mysql->query("INSERT INTO transacoes (BancoOrigem, AgenciaOrigem, ContaOrigem, BancoDestino, AgenciaDestino, ContaDestino, Valor, DataeHora,
-            Usuario ) VALUES ('$BancoOrigem', '$AgenciaOrigem', '$ContaOrigem', '$BancoDestino', '$AgenciaDestino', '$ContaDestino',
-            '$Valor', '$DataeHora', '$usuario')");
-        }
+                $result = $mysql->query("INSERT INTO transacoes (BancoOrigem, AgenciaOrigem, ContaOrigem, BancoDestino, AgenciaDestino, ContaDestino, Valor, DataeHora,
+                Usuario ) VALUES ('$BancoOrigem', '$AgenciaOrigem', '$ContaOrigem', '$BancoDestino', '$AgenciaDestino', '$ContaDestino',
+                '$Valor', '$DataeHora', '$usuario')");
+            }
 
 
             
-        if ($result){
-        echo "Dados inseridos com sucesso !!!";
-            
-        }else {
-                echo "Ocorreu um erro ao inserir os dados";
-        }
+            if ($result){
+            echo "Dados inseridos com sucesso !!!";
+                
+            }else {
+                    echo "Ocorreu um erro ao inserir os dados";
+            }
     }     
         
 
