40c22f3d61e320ae59d42ba303968e203df85f01
Cristina <98393518+crismgsp@users.noreply.github.com>
2022-07-26 20:38:45 -0300

    Update analisefinanceira.php

--- classesEsimilares/analisefinanceira.php
+++ classesEsimilares/analisefinanceira.php
@@ -100,7 +100,9 @@ class Analise
         return $contasuspeita;
     }
 
-    public function agenciaSuspeita(): array
+    public function agenciaSuspeita(): array   //checar melhor esta pedaço...na hora de criar a chave...acho que fiz errado...é que dos testes que fiz
+       // nenhum tinha detectado, irei refazer outros testes...e se ver que precisa mudar atualizo este pedaço aqui... na parte da chave nao deve ser assim
+        // $chave = $dados['Banco'].$dados['Agencia'].$dados['Conta']; tem que ser so $dados['Banco'].$dados['Agencia'] pois está checando agencia e nao conta
     { 
 
         
